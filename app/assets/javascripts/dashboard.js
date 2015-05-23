9f30637a973dfefcee0a4630e3198f7323510db7
vinkal <vinkal087@gmail.com>
2015-05-24 03:45:08 +0530

    Refactor

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,18 +1,11 @@
 function getDerivedImages(id){
 	//alert(id);
 	$.get("/dashboard/listderivedimages/"+id, function(data){
-		
-		
-		
 		var obj = data
-		
-        $('#derivedimages').empty()
-       
-        //$('#derivedimages').append(new Option("--Select--","select"))
-       for(var i=0;i<obj.length;i++){
-        $('#derivedimages').append(new Option(obj[i]['description'],obj[i]['id']))
-        }
-
+		$('#derivedimages').empty()
+    for(var i=0;i<obj.length;i++){
+      $('#derivedimages').append(new Option(obj[i]['description'],obj[i]['id']))
+    }
 	});
 }
 $(document).ready(function () {
@@ -23,13 +16,14 @@ $(document).ready(function () {
     cvmactiondropdown();
 $( ".slider" ).slider( "values", [ 55, 105 ] );
  });
+
 function edithost(){
   $('#hosttablediv').hide();
   $('#statisticsdiv').hide();
   $('#edithostdiv').show();
   $('#addhostdiv').hide();
-  
 }
+
 function gethosts(){
   $('#hosttablediv').show();
   $('#statisticsdiv').hide();
@@ -48,11 +42,8 @@ function getstatistics(){
   $('#hosttablediv').hide();
   $('#statisticsdiv').show();
   $('#edithostdiv').hide();
-  $('#addhostdiv').hide();
-  
+  $('#addhostdiv').hide();  
 }
-
-
 
 function getterminal(){
   $('#terminalpanel').attr('src',"");
@@ -60,42 +51,34 @@ function getterminal(){
   var ip  = "http://"+data[1];
   var port = parseInt(data[0]) + 25001;
   var url = (ip+":"+port);
-  $('#terminalpanel').attr('src',url);
-     
-   $('#tablediv').hide();
-    $('#detailspanel').hide();
-   $('#terminalpanel').show();
-   
+  $('#terminalpanel').attr('src',url);   
+  $('#tablediv').hide();
+  $('#detailspanel').hide();
+  $('#terminalpanel').show();
 }
+
 function getdetails(){
 	var id = $('#cvm_radio:checked').val();
-   //$.get("/dashboard/listderivedimages/"+id, function(data){
-   	
-   //});
    $('#terminalpanel').hide();
    $('#tablediv').hide();
    $('#detailspanel').show();
 }
 
 function cvmlist(){
-	
-     
      $('#terminalpanel').hide();
-       cvmactiondropdown();         
+     cvmactiondropdown();         
      $('#detailspanel').hide();
      $('#tablediv').show();
 }
 function cvmactiondropdown()
-{   $("tr").click(function () {
-                //alert('Table row clicked');
-                $(this).children().find(":radio").attr('checked', 'checked');
-            }) 
+{   
+  $("tr").click(function () {
+      $(this).children().find(":radio").attr('checked', 'checked');
+      }) 
    
    $(".actionmenu").click(function(){
    	var data = $('#cvm_radio:checked').val().split('_');
-    
     var cvmid = parseInt(data[0]);
-   	
    	var operation = $(this).attr('value');
       $.get("/dashboard/operatecvm/"+cvmid+"/"+operation, function(data){
    	alert(data);
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -4,7 +4,6 @@ module DashboardHelper
   def get_base_images
 	  res = HTTParty.get(APP_CONFIG['REST_API']['SERVER_NAME']+'/api/getbaseimages')
     @base_images = JSON.parse(res.body)
-    #puts @base_images
   end
  
   def get_derived_images
